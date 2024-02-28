aa0c537263f3f000fa225162ee7ceef6230069a0
Andrew M. <amuratha@tuta.io>
2024-02-28 22:01:12 +0300

    feat(test): add unit tests
    chore: re-arrange private functions
    
    Added unit tests for game initialisation and making moves

--- lib/game.ex
+++ lib/game.ex
@@ -3,7 +3,6 @@ defmodule Xando.Game do
 
   def init_game(grid_size) do
     %{
-      result: "",
       last_move: :nil,
       next_move: :nil,
       comment: {:ok, ""},
@@ -14,14 +13,6 @@ defmodule Xando.Game do
     }
   end
 
-  defp init_game_board(grid_size) do
-    for row <- 0..grid_size - 1,
-      col <- 0..grid_size - 1 do
-        {row, col}
-    end
-    |> Map.from_keys(" ")
-  end
-
   def play(player, move, game) do
      if not game.complete and can_play?(player, game, move) do
       new_game = Map.put(game, :board, Map.put(game.board, move, player))
@@ -29,80 +20,18 @@ defmodule Xando.Game do
       |> Map.put(:next_move, toggle_player(player))
       |> Map.put(:remaining_moves, game.remaining_moves - 1)
 
-      if victory?(player, new_game) do
-        Map.put(new_game, :comment, {:ok, ~s"Game complete, #{player} wins!"})
-        |> Map.put(:result, ~s"#{player} wins!")
-        |> Map.put(:complete, true)
-      else
-        if game.remaining_moves <= 0 do
-          Map.put(new_game, :comment, {:ok, "Stalemate, no one wins!"})
-          |> Map.put(:result, "Stalemate")
+      case victory?(player, new_game) do
+        {:ok, true, comment} -> true
+          Map.put(new_game, :comment, comment)
           |> Map.put(:complete, true)
-        else
-          Map.put(new_game, :comment, {:ok, ~s"Next move #{toggle_player(player)}"})
-        end
+        _ ->
+          Map.put(new_game, :comment, ~s"next move #{toggle_player(player)}")
       end
     else
       Map.put(game, :comment, {:error, "Invalid move"})
     end
   end
   
-  def victory?(player, game) do
-    check_victory_columns?(false, player, game)
-    |> check_victory_rows?(game.board, game.grid_size)
-    |> check_victory_right_diagonal?(player, game)
-    |> check_victory_left_diagonal?(player, game)
-  end
-  
-  defp check_victory_columns?(checked, player, game) do
-    if not checked do
-      col_regex = ~r/(#{player}[#{player}#{toggle_player(player)}\s]{#{game.grid_size - 1}}){#{game.grid_size - 1}}#{player}/
-      board_str = Map.values(game.board) |> List.to_string
-      Regex.match?(col_regex, board_str)
-    else
-      checked
-    end
-  end
-
-  defp check_victory_rows?(checked, board, grid_size) do
-    if not checked do
-      Enum.chunk_every(Map.values(board), grid_size)
-      |> Enum.filter(fn x -> MapSet.new(x) != MapSet.new([" "]) end)
-      |> Enum.any?(fn x -> MapSet.size(MapSet.new(x)) == 1 end)
-    else
-      checked
-    end
-  end
-
-  defp check_victory_right_diagonal?(checked, player, game) do
-    if not checked do
-      r_diag = Enum.filter(Map.keys(game.board), fn {x, y} ->
-        x == y and
-        Map.get(game.board, x) != " "
-      end)
-      |> Enum.map(fn x -> Map.get(game.board, x) end)
-      |> List.to_string
-      Regex.match?(~r/^[#{player}]{game.grid_size}$/, r_diag)
-    else
-      checked
-    end
-  end
-
-  defp check_victory_left_diagonal?(checked, player, game) do
-    if not checked do
-      l_diag = Enum.filter(Map.keys(game.board), fn {x, y} ->
-        y == 1 and 0 < x and x < game.grid_size - 1
-      end)
-      |> Kernel.++([{0, game.grid_size - 1}, {game.grid_size - 1, 0}])  # Add corner coordinates
-      |> Enum.filter(fn x -> Map.get(game.board, x) != " " end)
-      |> Enum.map(fn x -> Map.get(game.board, x) end)
-      |> List.to_string
-      Regex.match?(~r/^[#{player}]{game.grid_size}$/, l_diag)
-    else
-      checked
-    end
-  end
-
   def toggle_player(player) do
     case player do
       "X" -> "O"
@@ -128,6 +57,109 @@ defmodule Xando.Game do
     |> Table.put_column_meta(:all, align: :center)
     |> Table.render!(horizontal_style: :all)
     |> IO.puts
+  end
+
+  def victory?(player, game) do
+    check_stalemate?(game)
+    |> check_victory_columns?(player, game)
+    |> check_victory_rows?(player, game)
+    |> check_victory_right_diagonal?(player, game)
+    |> check_victory_left_diagonal?(player, game)
+  end
+ 
+  #
+  # Private Functions
+  #
+
+  defp init_game_board(grid_size) do
+    for row <- 0..grid_size - 1,
+      col <- 0..grid_size - 1 do
+        {row, col}
+    end
+    |> Map.from_keys(" ")
+  end
+
+  defp check_stalemate?(game) do
+    stalemate = game.remaining_moves == 0
+    {:ok, stalemate, if stalemate do
+        "stalemate"
+      else
+        ""
+    end}
+  end
+
+  defp check_victory_columns?(checked, player, game) do
+    case checked do
+      {:ok, false, _} -> 
+        match_rgx = Regex.match?(
+          ~r/(#{player}[#{player}#{toggle_player(player)}\s]{#{game.grid_size - 1}}){#{game.grid_size - 1}}#{player}/,
+          Map.values(game.board) |> List.to_string
+        )
+
+        {:ok, match_rgx, if match_rgx do
+            ~s"#{player} wins by column"
+        else
+            ""
+        end}
+      _ -> checked
+    end
+  end
+
+  defp check_victory_rows?(checked, player, game) do
+    case checked do
+      {:ok, false, _} ->
+        row_victory = Enum.chunk_every(Map.values(game.board), game.grid_size)
+          |> Enum.filter(fn x -> MapSet.new(x) != MapSet.new([" "]) end)
+          |> Enum.any?(fn x -> MapSet.size(MapSet.new(x)) == 1 end)
+        
+        {:ok, row_victory, if row_victory do
+            ~s"#{player} wins by row"
+          else
+            ""
+        end}
+      _ -> checked
+    end
+  end
+
+  defp check_victory_right_diagonal?(checked, player, game) do
+    case checked do
+      {:ok, false, _} ->
+        r_diag = Enum.filter(Map.keys(game.board), fn {x, y} ->
+            x == y and
+            Map.get(game.board, x) != " "
+          end)
+          |> Enum.map(fn x -> Map.get(game.board, x) end)
+          |> List.to_string
+        r_diag_victory = Regex.match?(~r/^[#{player}]{game.grid_size}$/, r_diag)
+
+        {:ok, r_diag_victory, if r_diag_victory do
+            ~s"#{player} wins by right diagonal"
+          else
+            ""
+        end}
+      _ -> checked
+    end
+  end
+
+  defp check_victory_left_diagonal?(checked, player, game) do
+    case checked do
+      {:ok, false, _} ->
+        l_diag = Enum.filter(Map.keys(game.board), fn {x, y} ->
+          y == 1 and 0 < x and x < game.grid_size - 1
+        end)
+        |> Kernel.++([{0, game.grid_size - 1}, {game.grid_size - 1, 0}])  # Add corner coordinates
+        |> Enum.filter(fn x -> Map.get(game.board, x) != " " end)
+        |> Enum.map(fn x -> Map.get(game.board, x) end)
+        |> List.to_string
+        l_diag_victory = Regex.match?(~r/^[#{player}]{game.grid_size}$/, l_diag)
+
+        {:ok, l_diag_victory, if l_diag_victory do
+            ~s"#{player} wins by left diagonal"
+          else
+            ""
+        end}
+      _ -> checked
+    end
   end
 
 end
--- test/xando_test.exs
+++ test/xando_test.exs
@@ -2,7 +2,39 @@ defmodule XandoTest do
   use ExUnit.Case
   doctest Xando
 
-  test "greets the world" do
-    assert Xando.hello() == :world
+  alias Xando.Game
+
+  setup_all do
+    {:ok, grid_size: 4}
   end
+
+  test "can initialise game", state do
+    game = Game.init_game(state[:grid_size])
+
+    assert game.complete == false
+    assert game.remaining_moves == state[:grid_size] * state[:grid_size]
+    assert Map.keys(game.board) == Enum.map(
+      0..state[:grid_size] - 1, fn x ->
+        Enum.map(
+          0..state[:grid_size] - 1, fn y ->
+            {x, y}
+          end)
+      end)
+      |> List.flatten
+  end
+
+  test "can make a moves" do
+    game = Game.init_game(3)
+    game = Game.play("X", {0, 0}, game)
+    game = Game.play("O", {1, 1}, game)
+    game = Game.play("X", {2, 2}, game)
+    boad_str = Map.values(game.board) |> List.to_string
+
+    assert game.complete == false
+    assert game.last_move == "X"
+    assert game.next_move == "O"
+    assert game.comment == "next move O"
+    assert boad_str == "X   O   X"
+  end
+
 end
