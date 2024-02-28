bc5c8b28d9cf0e14dbc917fe183974b9378bf5ab
Andrew M. <amuratha@tuta.io>
2024-02-28 13:49:45 +0300

    feat: rework board init and victory conditions
    Rework board initialisation to use Map and Tuple keys. Simplifies
    cell checking and setting.
    
    Rework victory conditions checks. Stallmate check is broken.

--- lib/game.ex
+++ lib/game.ex
@@ -1,49 +1,106 @@
 defmodule Xando.Game do
   alias TableRex.Table
 
-  def init_game() do
+  def init_game(grid_size) do
     %{
       result: "",
       last_move: :nil,
       next_move: :nil,
       comment: {:ok, ""},
       complete: false,
-      board: {
-        "_", "_", "_",
-        "_", "_", "_",
-        "_", "_", "_"
-      }
+      grid_size: grid_size,
+      remaining_moves: grid_size * grid_size,
+      board: init_game_board(grid_size)
     }
   end
 
-  def play(player, move, game) do
-    index = translate_move(move)
+  defp init_game_board(grid_size) do
+    for row <- 0..grid_size - 1,
+      col <- 0..grid_size - 1 do
+        {row, col}
+    end
+    |> Map.from_keys(" ")
+  end
 
-    if not game.complete and can_play?(player, game, index) do
-      new_game = Map.put(game, :board, put_elem(game.board, index, player))
+  def play(player, move, game) do
+     if not game.complete and can_play?(player, game, move) do
+      new_game = Map.put(game, :board, Map.put(game.board, move, player))
       |> Map.put(:last_move, player)
       |> Map.put(:next_move, toggle_player(player))
+      |> Map.put(:remaining_moves, game.remaining_moves - 1)
 
       if victory?(player, new_game) do
-        Map.put(game, :comment, {:ok, ~s"Game complete, #{player} wins!"})
+        Map.put(new_game, :comment, {:ok, ~s"Game complete, #{player} wins!"})
         |> Map.put(:result, ~s"#{player} wins!")
         |> Map.put(:complete, true)
       else
-        Map.put(new_game, :comment, {:ok, "continue..."})
+        if game.remaining_moves <= 0 do
+          Map.put(new_game, :comment, {:ok, "Stalemate, no one wins!"})
+          |> Map.put(:result, "Stalemate")
+          |> Map.put(:complete, true)
+        else
+          Map.put(new_game, :comment, {:ok, ~s"Next move #{toggle_player(player)}"})
+        end
       end
     else
       Map.put(game, :comment, {:error, "Invalid move"})
     end
   end
-
+  
   def victory?(player, game) do
-    col = ~r/(#{player}[^#{player}]{2}#{player}[^#{player}]{2}#{player})/
-    row = ~r/([#{player}]{3}+)/
-    l_diagonal = ~r/(#{player}[^#{player}]{3}#{player}[^#{player}]{3}#{player})/
-    r_diagonal = ~r/(#{player}[^#{player}]{1}#{player}[^#{player}]{1}#{player})/
+    check_victory_columns?(false, player, game)
+    |> check_victory_rows?(game.board, game.grid_size)
+    |> check_victory_right_diagonal?(player, game)
+    |> check_victory_left_diagonal?(player, game)
+  end
+  
+  defp check_victory_columns?(checked, player, game) do
+    if not checked do
+      col_regex = ~r/(#{player}[#{player}#{toggle_player(player)}\s]{#{game.grid_size - 1}}){#{game.grid_size - 1}}#{player}/
+      board_str = Map.values(game.board) |> List.to_string
+      Regex.match?(col_regex, board_str)
+    else
+      checked
+    end
+  end
 
-    board_str = Tuple.to_list(game.board) |> List.to_string
-    Enum.any?([col, row, l_diagonal, r_diagonal], fn x -> Regex.match?(x, board_str) end)
+  defp check_victory_rows?(checked, board, grid_size) do
+    if not checked do
+      Enum.chunk_every(Map.values(board), grid_size)
+      |> Enum.filter(fn x -> MapSet.new(x) != MapSet.new([" "]) end)
+      |> Enum.any?(fn x -> MapSet.size(MapSet.new(x)) == 1 end)
+    else
+      checked
+    end
+  end
+
+  defp check_victory_right_diagonal?(checked, player, game) do
+    if not checked do
+      r_diag = Enum.filter(Map.keys(game.board), fn {x, y} ->
+        x == y and
+        Map.get(game.board, x) != " "
+      end)
+      |> Enum.map(fn x -> Map.get(game.board, x) end)
+      |> List.to_string
+      Regex.match?(~r/^[#{player}]{game.grid_size}$/, r_diag)
+    else
+      checked
+    end
+  end
+
+  defp check_victory_left_diagonal?(checked, player, game) do
+    if not checked do
+      l_diag = Enum.filter(Map.keys(game.board), fn {x, y} ->
+        y == 1 and 0 < x and x < game.grid_size - 1
+      end)
+      |> Kernel.++([{0, game.grid_size - 1}, {game.grid_size - 1, 0}])  # Add corner coordinates
+      |> Enum.filter(fn x -> Map.get(game.board, x) != " " end)
+      |> Enum.map(fn x -> Map.get(game.board, x) end)
+      |> List.to_string
+      Regex.match?(~r/^[#{player}]{game.grid_size}$/, l_diag)
+    else
+      checked
+    end
   end
 
   def toggle_player(player) do
@@ -54,29 +111,18 @@ defmodule Xando.Game do
     end 
   end
 
-  def can_play?(player, game, index) do
-    case {elem(game.board, index), game.next_move == :nil or player == game.next_move}  do
-      {"_", true} -> true
+  def can_play?(player, game, move) do
+    case {
+      Map.get(game.board, move),
+      (game.next_move == :nil or player == game.next_move)
+    }  do
+      {" ", true} -> true
       _ -> false
     end
   end
 
-  def translate_move(move) do
-    case move do
-      "a1" -> 0
-       "b1" -> 1
-       "c1" -> 2
-       "a2" -> 3
-       "b2" -> 4
-       "c2" -> 5
-       "a3" -> 6
-       "b3" -> 7
-       _ -> 8 # Assume that anything else is the last element
-    end
-  end
-
   def print_board(board) do
-    Tuple.to_list(board)
+    Map.values(board)
     |> Enum.chunk_every(3)
     |> Table.new([])
     |> Table.put_column_meta(:all, align: :center)
--- lib/xando.ex
+++ lib/xando.ex
@@ -1,18 +1,3 @@
 defmodule Xando do
-  @moduledoc """
-  Documentation for `Xando`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Xando.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
